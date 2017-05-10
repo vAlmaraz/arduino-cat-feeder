44c50e6169774251172a0ded2f6715928731225a
Víctor Almaraz <unvictor@gmail.com>
2017-05-10 22:08:28 +0200

    Three daily alarms feeding a constant feed unit. Log showing info

--- cat_feeder.ino
+++ cat_feeder.ino
@@ -1,65 +1,95 @@
-// https://www.youtube.com/watch?v=7DwUOdvJp-8
-// https://circuits.io/circuits/2707080
-// https://gist.github.com/ulitiy/5c319f65c419dfa1af7ca2dcdebab3ce
+/**
+ * CAT FEEDER
+ * 
+ * A way to automate pet feed.
+ * 
+ * Configuration:
+ * Servo:
+ * - Red: 5v
+ * - Black: Ground
+ * - White: PIN_SERVO_AUTO_FEED
+ * Clock:
+ * - SDA (right side): A4
+ * - SCL (right side): A5
+ * 
+ * Links:
+ * https://www.youtube.com/watch?v=7DwUOdvJp-8
+ * https://circuits.io/circuits/2707080
+ * https://gist.github.com/ulitiy/5c319f65c419dfa1af7ca2dcdebab3ce
+ * https://www.pjrc.com/teensy/td_libs_TimeAlarms.html
+ */
+ 
 #include <Servo.h>
+#include <Time.h>
+#include <TimeLib.h>
+#include <TimeAlarms.h>
 
 Servo myservo;
 
-const int SERVO_PIN = 9;
-const int BUTTON_PIN = 12;
-const int LED_PIN = 13;
-const int DEFAULT_FEED_UNITS = 3;
+const int PIN_SERVO_AUTO_FEED = 9;
+const int DEFAULT_AUTO_FEED_UNITS = 2;
+
+const bool SHOW_LOG = true;
+
+bool feeding = false;
 
 void setup() {
-  setupServo();
+  if (SHOW_LOG) {
+    Serial.begin(9600);
+  }
   setupClock();
+  setupServo();
 }
 
 void loop() {
-  int buttonVal = digitalRead(BUTTON_PIN);
-  if(buttonVal == LOW) {
-    feed(DEFAULT_FEED_UNITS);
+  if (SHOW_LOG) {
+    digitalClockDisplay();
   }
-  delay(13);
+  Alarm.delay(1000);
 }
 
 // SETUPS
 
+void setupClock() {
+  setTime(07, 29, 50, 10, 2, 17);
+  Alarm.alarmRepeat(07, 30, 0, morningAlarm);
+  Alarm.alarmRepeat(07, 30, 30, afternoonAlarm);
+  Alarm.alarmRepeat(07, 31, 0, nightAlarm);
+}
+
 void setupServo() {
-  myservo.attach(SERVO_PIN);
-  pinMode(BUTTON_PIN, INPUT);
-  digitalWrite(BUTTON_PIN, HIGH);
-  pinMode(LED_PIN, OUTPUT);
-  digitalWrite(LED_PIN, LOW);
+  myservo.attach(PIN_SERVO_AUTO_FEED);
   myservo.write(180);
   delay(1000);
   myservo.detach();
 }
 
-void setupClock() {
-  setTime(7, 29, 0, 10, 2, 17); 
-  Alarm.alarmRepeat(7, 30, 0, morningAlarm);
-  Alarm.alarmRepeat(15, 00, 0, afternoonAlarm);
-  Alarm.alarmRepeat(23, 30, 0, nightAlarm);
-}
-
 // ALARMS
 
+void digitalClockDisplay() {
+  // digital clock display of the time
+  showLog(String(hour()) + timeDigitToString(minute()) + timeDigitToString(second()));
+}
+
 void morningAlarm() {
-  feed(DEFAULT_FEED_UNITS);
+  showLog("morningAlarm"); 
+  feed(DEFAULT_AUTO_FEED_UNITS);
 }
 
 void afternoonAlarm() {
-  feed(DEFAULT_FEED_UNITS);
+  showLog("afternoonAlarm"); 
+  feed(DEFAULT_AUTO_FEED_UNITS);
 }
 
 void nightAlarm() {
-  feed(DEFAULT_FEED_UNITS);
+  showLog("nightAlarm"); 
+  feed(DEFAULT_AUTO_FEED_UNITS);
 }
 
 // FEED
 
 void feed(int units) {
+  showLog("About to feed " + String(units) + " units");
   for (int i = 0; i < units; i++) {
     feedOneUnit();
     delay(500);
@@ -67,11 +97,35 @@ void feed(int units) {
 }
 
 void feedOneUnit() {
-  myservo.attach(SERVO_PIN);
+  if (feeding) {
+    showLog("Invalid feed. It is actually feeding");
+    return;
+  }
+  showLog("Feeding...");
+  feeding = true;
+  myservo.attach(PIN_SERVO_AUTO_FEED);
   myservo.write(0);
   delay(400);
   myservo.write(180);
   delay(1500);
   myservo.detach();
+  feeding = false;
+}
+
+// UTILS
+
+String timeDigitToString(int digits) {
+  String digitsStr = ":";
+  if (digits < 10) {
+    digitsStr += "0";
+  }
+  digitsStr += digits;
+  return digitsStr;
+}
+
+void showLog(String message) {
+  if (SHOW_LOG) {
+    Serial.println(message);
+  }
 }
 
